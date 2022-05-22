4f32816031916dc15fd0d29d7bb601c2e9fa7dce
Mateus Nadal <nadalmateus@hotmail.com>
2022-05-21 22:49:52 -0300

    feat: added a static method to use as data persistence strategy, just to test

--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -4,29 +4,38 @@ namespace ECommerce.Repositories
 {
     public class UserRepository : IUserRepository
     {
+        private static List<User> _db = new List<User>()
+    {
+        new User(){Id = 1, Name = "Matt1", Email = "matt1@matt.com"},
+        new User(){Id = 2, Name = "Matt2", Email = "matt2@matt.com"},
+        new User(){Id = 3, Name = "Matt3", Email = "matt3@matt.com"}
+    };
+
+
         public List<User> GetAllUsers()
         {
-            throw new NotImplementedException();
+            return _db;
         }
 
         public User GetUserById(int id)
         {
-            throw new NotImplementedException();
+            return _db.FirstOrDefault(a => a.Id == id);
         }
 
         public void CreateUser(User user)
         {
-            throw new NotImplementedException();
+            _db.Add(user);
         }
 
         public void UpdateUser(User user)
         {
-            throw new NotImplementedException();
+            _db.Remove(_db.FirstOrDefault(a => a.Id == user.Id));
+            _db.Add(user);
         }
 
         public void DeleteUser(int id)
         {
-            throw new NotImplementedException();
+            _db.Remove(_db.FirstOrDefault(a => a.Id == id));
         }
     }
 }
